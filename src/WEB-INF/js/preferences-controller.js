e07be7306d510ae33faf40d812d610a017a4f6df
AyuCalices <58752514+AyuCalices@users.noreply.github.com>
2022-12-15 13:26:07 +0100

    updated data when switching to preferences-controller.js

--- src/WEB-INF/js/preferences-controller.js
+++ src/WEB-INF/js/preferences-controller.js
@@ -33,7 +33,61 @@ class PreferencesController extends Controller {
         sendButton.addEventListener("click", eventHandler);
         sendButton.addEventListener("touchstart", eventHandler);
 
+        // show data
+        this.activateSessionOwner(template, section);
+
         // Controller.sessionOwner = null;
+    }
+
+    activateSessionOwner(template, section) {
+        const sessionOwner = Controller.sessionOwner;
+        const elements = section.querySelectorAll("img, input");
+
+        console.log("session owner: ", sessionOwner);
+        console.log(JSON.stringify(Controller.sessionOwner))
+
+        //TODO: below seems to work ... (copied from skat)
+        elements[0].src = "/services/documents/" + sessionOwner.avatarReference + "?cache-bust=" + Date.now();
+
+        elements[1].value = sessionOwner.email;
+        elements[2].value = "";
+        elements[3].value = sessionOwner.name.title;
+        elements[4].value = sessionOwner.name.given;
+        elements[5].value = sessionOwner.name.family;
+        elements[6].value = sessionOwner.group;
+        elements[7].value = sessionOwner.address.street;
+        elements[8].value = sessionOwner.address.postcode;
+        elements[9].value = sessionOwner.address.city;
+        elements[10].value = sessionOwner.address.country;
+        elements[11].value = sessionOwner.account.bic;
+        elements[12].value = sessionOwner.account.iban;
+
+
+        //TODO: from skat viable ? test below for showing phone numbers!
+        const phoneInputsDom = document.querySelectorAll("head template.preferences fieldset.phones input")
+        const phoneInputsArray = Array.from(phoneInputsDom);
+        phoneInputsArray.forEach(e => e.remove());
+
+        for (const phone of sessionOwner.phones)
+            this.activateSessionOwnerPhone(phone);
+    }
+
+    /**
+     * Adds the given phone number to the list of phone numbers.
+     */
+    activateSessionOwnerPhone(phone) {
+
+        const phoneFieldset = document.querySelector("main > head template.preferences > fieldset:nth-of-type(4)");
+
+        const div = document.createElement("div");
+        const input = document.createElement("input");
+        input.value = phone;
+        div.append(input);
+
+        const buttonDiv = phoneFieldset.querySelector("div:last-of-type");
+        buttonDiv.remove();
+        phoneFieldset.append(div);
+        phoneFieldset.append(buttonDiv);
     }
 
 
